ece142787589be931d7b5bf53305c98fc528615c
seba-1511 <seba-1511@hotmail.com>
2015-01-29 01:44:13 -0800

    Finished linking dropbox to django and sending back the response.

--- fluster_backend/cluster/views.py
+++ fluster_backend/cluster/views.py
@@ -2,7 +2,7 @@ import json
 from dropbox.client import DropboxClient
 from django.http import HttpResponse
 from sklearn.cluster import KMeans
-from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from time import sleep
 
 
@@ -14,9 +14,9 @@ def get_files(client):
 
 def cluster_files(files, metadata):
     km = KMeans(n_clusters=2, init='k-means++')
-    vectorizer = TfidfVectorizer(max_df=0.5, max_features=200,
-                                 min_df=2, stop_words='english',
-                                 use_idf=True)
+    vectorizer = CountVectorizer(input='content', analyzer='word',
+                                 max_features=200, stop_words='english',
+                                 encoding='utf16')
     print 'Vectorizing files'
     X = vectorizer.fit_transform(files)
     print 'Fitting'
@@ -32,10 +32,11 @@ def launch_clustering(request, token):
     print 'Getting Client'
     client = DropboxClient(token)
     print 'Downloading the files'
-    files, metadata = get_files(client)
-    files = [f.read() for f in files]
+    # files, metadata = get_files(client)
+    # files = [f.read() for f in files]
     print 'Clustering init...'
-    cluster = cluster_files(files, metadata)
+    cluster = client.metadata('/')
+    # cluster = cluster_files(files, metadata)
     # Simulate upload work:
     sleep(3.0)
     return HttpResponse(json.dumps(cluster))

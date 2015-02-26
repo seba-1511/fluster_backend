0b7bf7e663970f06c4295bc1249a9dcfab2e49ca
Matthew <matthew.obrien929@gmail.com>
2015-02-25 17:58:02 -0800

    removed pipeline from views.py

--- fluster_backend/cluster/views.py
+++ fluster_backend/cluster/views.py
@@ -1,37 +1,5 @@
-import json
-import fluster
-from dropbox.client import DropboxClient
-from django.http import HttpResponse
-from sklearn.cluster import KMeans
-from sklearn.feature_extraction.text import TfidfVectorizer
+from pipeline import pipeline
 
 
 def launch_clustering(request, token):
-    """  
-    Call this to get files from dropbox, parse, cluster and return cluster results.
-
-    Parameters
-    ----------
-    token : Dropbox API token
-
-    Return Values
-    -------------
-    X : array-like, shape (n_samples, 2)
-        2D coordinates of resulting clusters
-
-    labels : array-like, shape (n_samples, )
-        Cluster assignments
-
-    """
-
-    db = fluster.Fluster(token)
-    paths = db.get_file_paths()
-    db.get_files_and_write(paths)
-    contents = db.parse_files_in_folder('dropbox_files/')
-
-    X = TfidfVectorizer().fit_transform(contents)
-    km = KMeans(n_clusters=2)
-    X = km.fit_transform(X)
-    labels = km.labels_
-    return X, labels
-
+   return pipeline(token)
